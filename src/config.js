b44cf9c57fdaa22c84cd4e1e781a2c839bc981d8
cavematt <mpotts207@gmail.com>
2020-09-29 18:48:32 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-2",
+      BUCKET: "practice-notes-upload"
+    },
+    apiGateway: {
+      REGION: "eu-west-2",
+      URL: "https://sldu6ui1y8.execute-api.eu-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-2",
+      USER_POOL_ID: "eu-west-2_sCR52nwRJ",
+      APP_CLIENT_ID: "6m2vl8jpg59mt4e8otkiuic2s",
+      IDENTITY_POOL_ID: "eu-west-2:84af3939-2fce-4a3f-910b-748c879e41b7"
+    }
+  };
